4d700de22af1b643eb51e76c862a8dbb7f36048e
FaizaSiddiqu <160780189+FaizaSiddiqu@users.noreply.github.com>
2024-09-30 22:04:06 +0500

    update file

--- chap-2..variables-for-string/index.js
+++ chap-2..variables-for-string/index.js
@@ -1,3 +1,43 @@
+    // --------------------Chapter # 2  (Variables for strings)----------------
+
+// 1--    Write code that declares a variable message, assigns the value "Hello, world!" to
+// it, and then displays an alert with that message
+
+let message = "Hello, world!";
+alert(message);
+
+//2--Declare a variable named age, leave it undefined, then assign the value 25 to it in a
+// later statement. Display an alert showing the value of age.
+
+let age ;   // Declaring the variable age without assigning a value
+alert(age);
+
+age = 25;    // Assigning the value 25 to the age variable
+alert(age);
+
+//3--Declare a variable named greeting, assign it the value "Good morning!", and then
+// display it in an alert. Afterwards, change the value of greeting to "Good evening!"
+// and display the new value in another alert.
+
+let greeting = "Good morning!";
+alert(greeting);
+
+greeting = "Good evening!"
+alert(greeting);
+
+// 4--Create a variable called favoriteColor and assign it the value "blue". Then display
+// an alert that says "My favorite color is blue" using the variable.
+
+let favoriteColor = "blue";
+alert(`My favorite color is ${favoriteColor}.`);
+
+
+
+
+
+
+// --------------------Chapter # 2  (Variables for strings)----------------
+
 // 1. Declare a variable called username.
 
 // var userName = "Jhon Doe";
