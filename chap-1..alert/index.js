6a6c155bb7fc2cb26b58948a0a39c14be060ed10
FaizaSiddiqu <160780189+FaizaSiddiqu@users.noreply.github.com>
2024-09-30 21:43:54 +0500

    update file

--- chap-1..alert/index.js
+++ chap-1..alert/index.js
@@ -1,3 +1,35 @@
+                //    -----------Chapter # 1 (alert)-------------
+
+//1-- What does the alert function do in JavaScript?
+// The alert() function in JavaScript is a built-in function that displays a modal window containing 
+// a message. This message is typically used to provide information, warnings, or errors to the user.
+// This code will display a popup window with the message "Hello, world!". The user must click the "OK"
+//  button to dismiss the alert and continue interacting with the page.
+
+// 2--Write the code to display an alert with the message "Welcome to my website!".
+
+alert("Welcome to my website!.");
+
+//3--What happens if you capitalize the alert keyword?
+// JavaScript is case-sensitive, meaning that keywords must be written in lowercase. If you use uppercase letters,
+//  the interpreter will not recognize it as a valid keyword and will throw an error.
+
+//4--Write the correct JavaScript syntax to display "Hello, world!" in an alert box.
+alert("Hello,World!.");
+
+// 5-- Write a JavaScript alert statement with proper formatting and explain each part of the code.
+
+// Explanation of each part:
+
+// alert: This is the keyword that identifies the built-in JavaScript function for displaying an alert message.
+// ( and ): These parentheses are required to enclose the argument passed to the alert function.
+// "This is a message!": This is the string literal that contains the message you want to display in the 
+// alert box. String literals are enclosed in double quotes (").
+
+
+
+ //    -----------Chapter # 1 (alert)-------------
+
 // 1. Write a script to greet your website visitor using JS alert box.
 
 // alert("Hello World");
